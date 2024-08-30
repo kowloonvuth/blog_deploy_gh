a4c85cae3f1343c752057562f0d4ddb993e9a0a8
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-08-30 19:52:08 +0700

    React router beginner project with hooks and links

---
+++ src/Components/PostPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
+
+const PostPage = ({posts, handleDelete }) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+
+  return (
+    <main className='PostPage'>
+        <article className='post'>
+          {post && 
+            <>
+              <h2>{post.title}</h2>
+              <p className='postData'>{post.datetime}</p>
+              <p className='postBody'>{post.body}</p>
+              <button onClick={() => handleDelete(post.id)}>
+                Delete Post
+              </button>
+            </>
+          }
+        </article>
+    </main>
+  )
+}
+
+export default PostPage
